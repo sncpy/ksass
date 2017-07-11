8fe9ffea2e9a4164d2535e0eaaf18f319f755f5e
juan.benitez <juan.benitez@konecta.com.py>
2017-07-11 14:20:40 -0400

    Se agregan directivas y templates para pantallas de filtrado con grillas, formularios y visualizacion

---
+++ src/js/directives/base-directive.js
@@ -0,0 +1,54 @@
+/**
+ * @mail     <a href="mailto:juan.benitez@konecta.com.py"/>
+ * @author   <a juan benitez</>
+ */
+
+/**
+ * directiva Base para las grillas con filtrado
+ */
+app.directive('listaDirective', function () {
+    return {
+        restrict: 'E',
+        transclude: {
+            'breadcrumb': 'breadcrumb',
+            'filtros': 'filtros',
+            'titulo': 'titulo',
+            'descripcion': 'descripcion',
+            'tabla': 'tabla'
+        },
+        replace: true,
+        templateUrl: '../partials/templates/list-template.html'
+    };
+});
+
+/**
+ * Directiva Base para los formularios de carga y edición
+ */
+app.directive('formDirective', function () {
+    return {
+        restrict: 'E',
+        transclude: {
+            'titulo': 'titulo',
+            'descripcion': 'descripcion',
+            'form': 'form'
+        },
+        replace: true,
+        templateUrl: '../partials/templates/form-template.html'
+    };
+});
+
+/**
+ * Directiva Base para los pantallas de visualizacion
+ */
+app.directive('viewDirective', function () {
+    return {
+        restrict: 'E',
+        transclude: {
+            'titulo': 'titulo',
+            'descripcion': 'descripcion',
+            'view': 'view'
+        },
+        replace: true,
+        templateUrl: '../partials/templates/view-template.html'
+    };
+});
